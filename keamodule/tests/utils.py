fe551930de34d3731c20d111c1651851ac7a3be8
jgmel <jgomez@phicus.es>
2024-02-11 17:57:12 +0100

    Refactor exception message construction in unit tests for method argument validations

--- keamodule/tests/utils.py
+++ keamodule/tests/utils.py
@@ -57,12 +57,10 @@ class BaseTestCase(unittest.TestCase):
     def assert_method_no_arguments(self, method):
         with self.assertRaises(TypeError) as cm:
             method(1)
-        msg = '%s() takes no arguments (1 given)' % method.__qualname__
-        self.assertEqual((msg,), cm.exception.args)
+        self.assertEqual((f'{method.__name__}() takes no arguments (1 given)',), cm.exception.args)
         with self.assertRaises(TypeError) as cm:
             method(x=1)
-        msg = '%s() takes no keyword arguments' % method.__qualname__
-        self.assertEqual((msg,), cm.exception.args)
+        self.assertEqual((f'{method.__name__}() takes no keyword arguments',), cm.exception.args)
 
     def assert_method_one_arg_no_keywords(self, method):
         with self.assertRaises(TypeError) as cm:
