c19f7361896cdc4125ffb288d2f6c03dce828387
jgmel <jgomez@phicus.es>
2024-07-09 17:20:34 +0200

    Feature #6 add type annotations for keamodule
    - Created `kea.pyi` file with type annotations for constants and classes.
    - Added `py.typed` marker file to indicate that the package includes type hints.
    - Updated `setup.py.in` to include `py.typed` and `kea.pyi` in package data.

---
+++ keamodule/kea.pyi
@@ -0,0 +1,323 @@
+from typing import Any
+
+from _typeshed import Incomplete
+
+ALL_SOURCES: int
+ALTERNATE_SOURCES: int
+BOOTREPLY: int
+BOOTREQUEST: int
+DHCP4_CLIENT_PORT: int
+DHCP4_SERVER_PORT: int
+DHCPACK: int
+DHCPBULKLEASEQUERY: int
+DHCPDECLINE: int
+DHCPDISCOVER: int
+DHCPINFORM: int
+DHCPLEASEACTIVE: int
+DHCPLEASEQUERY: int
+DHCPLEASEQUERYDONE: int
+DHCPLEASEQUERYSTATUS: int
+DHCPLEASEUNASSIGNED: int
+DHCPLEASEUNKNOWN: int
+DHCPNAK: int
+DHCPOFFER: int
+DHCPRELEASE: int
+DHCPREQUEST: int
+DHCPTLS: int
+DHCP_NOTYPE: int
+DHCP_OPTIONS_COOKIE: int
+DHCP_TYPES_EOF: int
+DHO_6RD: int
+DHO_ALL_SUBNETS_LOCAL: int
+DHO_ARP_CACHE_TIMEOUT: int
+DHO_ASSOCIATED_IP: int
+DHO_AUTHENTICATE: int
+DHO_AUTO_CONFIG: int
+DHO_BCMCS_DOMAIN_NAME_LIST: int
+DHO_BCMCS_IPV4_ADDR: int
+DHO_BOOT_FILE_NAME: int
+DHO_BOOT_SIZE: int
+DHO_BROADCAST_ADDRESS: int
+DHO_CAPWAP_AC_V4: int
+DHO_CLIENT_LAST_TRANSACTION_TIME: int
+DHO_COOKIE_SERVERS: int
+DHO_DEFAULT_IP_TTL: int
+DHO_DEFAULT_TCP_TTL: int
+DHO_DHCP_AGENT_OPTIONS: int
+DHO_DHCP_CLIENT_IDENTIFIER: int
+DHO_DHCP_LEASE_TIME: int
+DHO_DHCP_MAX_MESSAGE_SIZE: int
+DHO_DHCP_MESSAGE: int
+DHO_DHCP_MESSAGE_TYPE: int
+DHO_DHCP_OPTION_OVERLOAD: int
+DHO_DHCP_PARAMETER_REQUEST_LIST: int
+DHO_DHCP_REBINDING_TIME: int
+DHO_DHCP_RENEWAL_TIME: int
+DHO_DHCP_REQUESTED_ADDRESS: int
+DHO_DHCP_SERVER_IDENTIFIER: int
+DHO_DIRECTORY_AGENT: int
+DHO_DOMAIN_NAME: int
+DHO_DOMAIN_NAME_SERVERS: int
+DHO_DOMAIN_SEARCH: int
+DHO_END: int
+DHO_EXTENSIONS_PATH: int
+DHO_FINGER_SERVER: int
+DHO_FONT_SERVERS: int
+DHO_FQDN: int
+DHO_GEOCONF_CIVIC: int
+DHO_HOME_AGENT_ADDRS: int
+DHO_HOST_NAME: int
+DHO_IEEE802_3_ENCAPSULATION: int
+DHO_IMPRESS_SERVERS: int
+DHO_INTERFACE_MTU: int
+DHO_IP_FORWARDING: int
+DHO_IRC_SERVER: int
+DHO_LOG_SERVERS: int
+DHO_LPR_SERVERS: int
+DHO_MASK_SUPPLIER: int
+DHO_MAX_DGRAM_REASSEMBLY: int
+DHO_MERIT_DUMP: int
+DHO_NAME_SERVERS: int
+DHO_NAME_SERVICE_SEARCH: int
+DHO_NDI: int
+DHO_NDS_CONTEXT: int
+DHO_NDS_SERVERS: int
+DHO_NDS_TREE_NAME: int
+DHO_NETBIOS_DD_SERVER: int
+DHO_NETBIOS_NAME_SERVERS: int
+DHO_NETBIOS_NODE_TYPE: int
+DHO_NETBIOS_SCOPE: int
+DHO_NETINFO_ADDR: int
+DHO_NETINFO_TAG: int
+DHO_NISP_DOMAIN_NAME: int
+DHO_NISP_SERVER_ADDR: int
+DHO_NIS_DOMAIN: int
+DHO_NIS_SERVERS: int
+DHO_NNTP_SERVER: int
+DHO_NON_LOCAL_SOURCE_ROUTING: int
+DHO_NTP_SERVERS: int
+DHO_NWIP_DOMAIN_NAME: int
+DHO_NWIP_SUBOPTIONS: int
+DHO_PAD: int
+DHO_PANA_AGENT: int
+DHO_PATH_MTU_AGING_TIMEOUT: int
+DHO_PATH_MTU_PLATEAU_TABLE: int
+DHO_PCODE: int
+DHO_PERFORM_MASK_DISCOVERY: int
+DHO_POLICY_FILTER: int
+DHO_POP3_SERVER: int
+DHO_RDNSS_SELECT: int
+DHO_RESOURCE_LOCATION_SERVERS: int
+DHO_ROOT_PATH: int
+DHO_ROUTERS: int
+DHO_ROUTER_DISCOVERY: int
+DHO_ROUTER_SOLICITATION_ADDRESS: int
+DHO_SERVICE_SCOPE: int
+DHO_SIP_UA_CONF_SERVICE_DOMAINS: int
+DHO_SMTP_SERVER: int
+DHO_STATIC_ROUTES: int
+DHO_STDASERVER: int
+DHO_STREETTALK_SERVER: int
+DHO_SUBNET_MASK: int
+DHO_SUBNET_SELECTION: int
+DHO_SWAP_SERVER: int
+DHO_SYSTEM: int
+DHO_TCODE: int
+DHO_TCP_KEEPALIVE_GARBAGE: int
+DHO_TCP_KEEPALIVE_INTERVAL: int
+DHO_TFTP_SERVER_NAME: int
+DHO_TIME_OFFSET: int
+DHO_TIME_SERVERS: int
+DHO_TRAILER_ENCAPSULATION: int
+DHO_USER_AUTH: int
+DHO_USER_CLASS: int
+DHO_UUID_GUID: int
+DHO_V4_ACCESS_DOMAIN: int
+DHO_V4_CAPTIVE_PORTAL: int
+DHO_V4_LOST: int
+DHO_V4_PORTPARAMS: int
+DHO_VENDOR_CLASS_IDENTIFIER: int
+DHO_VENDOR_ENCAPSULATED_OPTIONS: int
+DHO_VIVCO_SUBOPTIONS: int
+DHO_VIVSO_SUBOPTIONS: int
+DHO_WWW_SERVER: int
+DHO_X_DISPLAY_MANAGER: int
+IDENT_CIRCUIT_ID: int
+IDENT_CLIENT_ID: int
+IDENT_DUID: int
+IDENT_FLEX: int
+IDENT_HWADDR: int
+NEXT_STEP_CONTINUE: int
+NEXT_STEP_DROP: int
+NEXT_STEP_PARK: int
+NEXT_STEP_SKIP: int
+PRIMARY_SOURCE: int
+RAI_OPTION_ACCESS_NETWORK_NAME: int
+RAI_OPTION_ACCESS_POINT_BSSID: int
+RAI_OPTION_ACCESS_POINT_NAME: int
+RAI_OPTION_ACCESS_TECHNO_TYPE: int
+RAI_OPTION_AGENT_CIRCUIT_ID: int
+RAI_OPTION_AUTH: int
+RAI_OPTION_DOCSIS_DEVICE_CLASS: int
+RAI_OPTION_LINK_SELECTION: int
+RAI_OPTION_OPERATOR_ID: int
+RAI_OPTION_OPERATOR_REALM: int
+RAI_OPTION_RADIUS: int
+RAI_OPTION_RELAY_FLAGS: int
+RAI_OPTION_RELAY_ID: int
+RAI_OPTION_RELAY_PORT: int
+RAI_OPTION_REMOTE_ID: int
+RAI_OPTION_SERVER_ID_OVERRIDE: int
+RAI_OPTION_SUBSCRIBER_ID: int
+RAI_OPTION_VIRTUAL_SUBNET_SELECT: int
+RAI_OPTION_VIRTUAL_SUBNET_SELECT_CTRL: int
+RAI_OPTION_VSI: int
+STATE_DECLINED: int
+STATE_DEFAULT: int
+STATE_EXPIRED_RECLAIMED: int
+UNSPECIFIED_SOURCE: int
+__version__: str
+logger: None
+
+class CalloutHandle:
+    def __init__(self, *args, **kwargs) -> None: ...
+    def deleteContext(self, *args, **kwargs): ...
+    def getArgument(self, *args, **kwargs): ...
+    def getContext(self, *args, **kwargs): ...
+    def getStatus(self, *args, **kwargs): ...
+    def setArgument(self, *args, **kwargs): ...
+    def setContext(self, *args, **kwargs): ...
+    def setStatus(self, *args, **kwargs): ...
+
+class CalloutManager:
+    use_count: Incomplete
+    def __init__(self, *args, **kwargs) -> None: ...
+
+class CfgMgr:
+    def __init__(self, *args, **kwargs) -> None: ...
+    def getCurrentCfg(self, *args, **kwargs): ...
+    def getStagingCfg(self, *args, **kwargs): ...
+
+class CfgSubnets4:
+    use_count: Incomplete
+    def __init__(self, *args, **kwargs) -> None: ...
+    def getAll(self, *args, **kwargs): ...
+    def getSubnet(self, *args, **kwargs): ...
+    def selectSubnet(self, *args, **kwargs): ...
+    def toElement(self, *args, **kwargs): ...
+
+class Host:
+    use_count: Incomplete
+    def __init__(self, *args, **kwargs) -> None: ...
+    def getHostId(self, *args, **kwargs): ...
+    def toElement(self, *args, **kwargs): ...
+
+class HostMgr:
+    @classmethod
+    def __init__(cls, *args, **kwargs) -> None: ...
+    def add(self, *args, **kwargs): ...
+    def del4(self, *args, **kwargs): ...
+    def del_(self, *args, **kwargs): ...
+    def get(self, *args, **kwargs): ...
+    def getAll4(self, *args, **kwargs): ...
+    def getPage4(self, *args, **kwargs): ...
+    @staticmethod
+    def instance(*args, **kwargs): ...
+
+class HostReservationParser4:
+    def __init__(self, *args, **kwargs) -> None: ...
+    def parse(self, *args, **kwargs): ...
+
+class Lease4:
+    addr: Incomplete
+    client_id: Incomplete
+    cltt: Incomplete
+    fqdn_fwd: Incomplete
+    fqdn_rev: Incomplete
+    hostname: Incomplete
+    hwaddr: Incomplete
+    state: Any
+    subnet_id: Incomplete
+    use_count: Incomplete
+    valid_lft: Incomplete
+    def __init__(self, *args, **kwargs) -> None: ...
+    def getContext(self, *args, **kwargs): ...
+    def setContext(self, *args, **kwargs): ...
+    def toElement(self, *args, **kwargs): ...
+
+class LeaseMgr:
+    def __init__(self, *args, **kwargs) -> None: ...
+    def addLease(self, *args, **kwargs): ...
+    def deleteLease(self, *args, **kwargs): ...
+    def getLease4(self, *args, **kwargs): ...
+    def getLeases4(self, *args, **kwargs): ...
+    def updateLease4(self, *args, **kwargs): ...
+    def wipeLeases4(self, *args, **kwargs): ...
+
+class LibraryHandle:
+    def __init__(self, *args, **kwargs) -> None: ...
+    def registerCommandCallout(self, *args, **kwargs): ...
+
+class Option:
+    use_count: Incomplete
+    def __init__(self, *args, **kwargs) -> None: ...
+    def addOption(self, *args, **kwargs): ...
+    def getBytes(self, *args, **kwargs): ...
+    def getOption(self, *args, **kwargs): ...
+    def getString(self, *args, **kwargs): ...
+    def getType(self, *args, **kwargs): ...
+    def getUint16(self, *args, **kwargs): ...
+    def getUint32(self, *args, **kwargs): ...
+    def getUint8(self, *args, **kwargs): ...
+    def pack(self, *args, **kwargs): ...
+    def setBytes(self, *args, **kwargs): ...
+    def setString(self, *args, **kwargs): ...
+    def setUint16(self, *args, **kwargs): ...
+    def setUint32(self, *args, **kwargs): ...
+    def setUint8(self, *args, **kwargs): ...
+    def toText(self, *args, **kwargs): ...
+
+class Pkt4:
+    use_count: Incomplete
+    def __init__(self, *args, **kwargs) -> None: ...
+    def addOption(self, *args, **kwargs): ...
+    def delOption(self, *args, **kwargs): ...
+    def getCiaddr(self, *args, **kwargs): ...
+    def getFlags(self, *args, **kwargs): ...
+    def getGiaddr(self, *args, **kwargs): ...
+    def getHWAddr(self, *args, **kwargs): ...
+    def getLocalAddr(self, *args, **kwargs): ...
+    def getOption(self, *args, **kwargs): ...
+    def getOptions(self, *args, **kwargs): ...
+    def getRemoteAddr(self, *args, **kwargs): ...
+    def getSiaddr(self, *args, **kwargs): ...
+    def getType(self, *args, **kwargs): ...
+    def getYiaddr(self, *args, **kwargs): ...
+    def pack(self, *args, **kwargs): ...
+    def setCiaddr(self, *args, **kwargs): ...
+    def setFlags(self, *args, **kwargs): ...
+    def setGiaddr(self, *args, **kwargs): ...
+    def setHWAddr(self, *args, **kwargs): ...
+    def setLocalAddr(self, *args, **kwargs): ...
+    def setRemoteAddr(self, *args, **kwargs): ...
+    def setSiaddr(self, *args, **kwargs): ...
+    def setType(self, *args, **kwargs): ...
+    def setYiaddr(self, *args, **kwargs): ...
+    def toText(self, *args, **kwargs): ...
+    def unpack(self, *args, **kwargs): ...
+
+class SrvConfig:
+    use_count: Incomplete
+    def __init__(self, *args, **kwargs) -> None: ...
+    def getCfgSubnets4(self, *args, **kwargs): ...
+    def toElement(self, *args, **kwargs): ...
+
+class Subnet4:
+    use_count: Incomplete
+    @classmethod
+    def __init__(cls, *args, **kwargs) -> None: ...
+    def getID(self, *args, **kwargs): ...
+    def getValid(self, *args, **kwargs): ...
+    def inRange(self, *args, **kwargs): ...
+    def toElement(self, *args, **kwargs): ...
+    def toText(self, *args, **kwargs): ...
